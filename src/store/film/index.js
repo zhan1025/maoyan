aed54f34ad65d5a6645744ea454274fc108d24f6
xiaoxunliu <1970801468@qq.com>
2019-06-15 08:34:33 +0800

    解决滚动条bug

--- src/store/film/index.js
+++ src/store/film/index.js
@@ -154,6 +154,7 @@ export default {
           })
         } else {
           Toast('兄弟，到底了')
+          commit('SETLOADING', false)
         }
       } else {
         axios.get('/my/ajax/movieOnInfoList?token=')
@@ -227,6 +228,7 @@ export default {
           })
         } else {
           Toast('你看个电影还想两年后看吗')
+          commit('SETLOADING', false)
         }
       } else {
         axios.get('/my/ajax/comingList?ci=30&token=&limit=10')
